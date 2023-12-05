8da098cdc2f35e5be3c2b07051ac545baff1651d
atheo810 <atheos810@gmail.com>
2023-12-05 21:18:42 +0800

    feat: TaskColumn

---
+++ src/components/TaskColumn.jsx
@@ -0,0 +1,13 @@
+import Todo from "../assets/direct-hit.png";
+import "./TaskColumn.css";
+
+export default function TaskColumn({ title, icon }) {
+  return (
+    <section className="task_column">
+      <h2 className="task_column_heading">
+        <img className="task_column_icon" src={icon} alt="" />
+        {title}
+      </h2>
+    </section>
+  );
+}
