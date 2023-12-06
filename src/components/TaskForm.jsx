4ce4b9a73d735ce2e96a360094b86046c716fda3
atheo810 <atheos810@gmail.com>
2023-12-06 10:20:16 +0800

    feat: implement useState

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,13 +1,50 @@
 import Tag from "./Tag";
 import "./TaskForm.css";
+
+import { useState } from "react";
+
 export default function TaskForm() {
+  // handle task input
+  // const [task, setTask] = useState("");
+  // const [status, setStatus] = useState("todo");
+
+  // function task input
+  // function handleTaskChange(e) {
+  //   setTask(e.target.value);
+  // }
+
+  // function handleStatusChange(e) {
+  //   setStatus(e.target.value);
+  // }
+
+  // console.log(task);
+
+  const [taskData, setTaskData] = useState({
+    task: "",
+    status: "todo",
+  });
+  function handleChange(e) {
+    const { name, value } = e.target;
+
+    setTaskData((prev) => {
+      return { ...prev, [name]: value };
+    });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(taskData);
+  }
   return (
     <header className="app_header">
-      <form action="">
+      <form action="" onSubmit={handleSubmit}>
         <input
           type="text"
+          name="task"
+          value={taskData.task}
           className="task_input"
           placeholder="Enter your Task"
+          onChange={handleChange}
         />
         <div className="task_form_bottom_line">
           <div>
@@ -17,12 +54,18 @@ export default function TaskForm() {
             <Tag tagName="React" />
           </div>
           <div>
-            <select className="task_status">
+            <select
+              className="task_status"
+              name="status"
+              onChange={handleChange}
+            >
               <option value="todo">To do</option>
               <option value="doing">Doing</option>
               <option value="done">Done</option>
             </select>
-            <button className="task_submit">+ Add Task</button>
+            <button className="task_submit" type="submit">
+              + Add Task
+            </button>
           </div>
         </div>
       </form>
