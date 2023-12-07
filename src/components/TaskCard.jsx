2b4212e735d134a578e8e4a5daa1628c32fed82d
atheo810 <atheos810@gmail.com>
2023-12-07 15:58:38 +0800

    feat: handle delete

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,21 +1,28 @@
-import "./TaskCard.css";
+import React from "react";
 
+import "./TaskCard.css";
 import Tag from "./Tag";
 import deleteIcon from "../assets/delete.png";
 
-export default function TaskCard() {
-  return (
-    <article className="task_card">
-      <p className="task_text">This is sample Text</p>
-      <div className="task_card_bottom_line">
-        <div className="task_card_tags">
-          <Tag tagName="HTML" />
-          <Tag tagName="CSS" />
-        </div>
-        <div className="task_delete">
-          <img src={deleteIcon} className="delete_icon" alt="" />
-        </div>
-      </div>
-    </article>
-  );
-}
+const TaskCard = ({ title, tags, handleDelete, index }) => {
+    return (
+        <article className='task_card'>
+            <p className='task_text'>{title}</p>
+
+            <div className='task_card_bottom_line'>
+                <div className='task_card_tags'>
+                    {tags.map((tag, index) => (
+                        <Tag key={index} tagName={tag} selected />
+                    ))}
+                </div>
+                <div
+                    className='task_delete'
+                    onClick={() => handleDelete(index)}>
+                    <img src={deleteIcon} className='delete_icon' alt='' />
+                </div>
+            </div>
+        </article>
+    );
+};
+
+export default TaskCard;
