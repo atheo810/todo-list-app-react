90dc5339f076963bf6e5642428c645fa6f1959c6
atheo810 <atheos810@gmail.com>
2023-12-07 16:03:15 +0800

    feat: mapping tasks

--- src/components/TaskColumn.jsx
+++ src/components/TaskColumn.jsx
@@ -1,15 +1,30 @@
+import React from "react";
 import Todo from "../assets/direct-hit.png";
-import TaskCard from "./TaskCard";
-import "./TaskColumn.css";
 
-export default function TaskColumn({ title, icon }) {
+import "./TaskColumn.css";
+import TaskCard from "./TaskCard";
+
+const TaskColumn = ({ title, icon, tasks, status, handleDelete }) => {
   return (
     <section className="task_column">
       <h2 className="task_column_heading">
-        <img className="task_column_icon" src={icon} alt="" />
-        {title}
+        <img className="task_column_icon" src={icon} alt="" /> {title}
       </h2>
-      <TaskCard />
+
+      {tasks.map(
+        (task, index) =>
+          task.status === status && (
+            <TaskCard
+              key={index}
+              title={task.task}
+              tags={task.tags}
+              handleDelete={handleDelete}
+              index={index}
+            />
+          )
+      )}
     </section>
   );
-}
+};
+
+export default TaskColumn;
